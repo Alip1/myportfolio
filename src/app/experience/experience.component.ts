12cb69881065a98c8b8078eff9e0284a29733dc7
Alip1 <muhammadali20june@gmail.com>
2023-05-02 22:26:31 +0200

    Experince component ready

--- src/app/experience/experience.component.ts
+++ src/app/experience/experience.component.ts
@@ -7,39 +7,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ExperienceComponent implements OnInit {
 
+
   public workExperiences = {
     viewExperiences: true,
     experience: [
       {
-        role: "Associate Full Stack Developer",
-        company: "DAOProptech",
-        color: "#3781c2",
-        link: "https://daoproptech.com/",
-        companylogo: "../../../assets/images/dao.png", //Need to update
-        date: "Aug 2021 – Dec 2022",
-        // desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
-        desc: "I have worked as a full stack web developer at DAOProptech, gaining experience in developing web applications and working in a team environment.", 
+        company: "Informationen zu KI-gestützten Assistenztechnologien",
+        color: '#3781c2',
+        },
+      {
+        company: "Wissenspool zum Thema KI & Inklusion",
+        color: '#3781c2',
       },
       {
-        role: "Software Engineer Intern",
-        company: "CUST University",
-        color: "#ff9102",
-        link: "https://cust.edu.pk/",
-        companylogo: "../../../assets/images/cust.png",
-        date: "Jul 2020 – Sep 2020",
-        desc: "The 8- Week summer training focused on mobile application development using (React Native) and helped learn to work in a team.",
-      },
-      {
-        role: "Computer Science & Engineering",
-        company: "GLA University",
-        color: "#3f703f",
-        companylogo: "../../../assets/images/gla.png",
-        date: "Aug 2016 – Jul 2020",
-        desc: "I have completed my 4 year graduation degree in computer science with overall CGPA 8.6",
-        // descBullets: [
-        //   "Lorem ipsum dolor sit amet, consectetur adipiscing elit",
-        //   "Lorem ipsum dolor sit amet, consectetur adipiscing elit"
-        // ]
+        company: "Projektaktivitäten (Praxislabore, Demonstrationsformate)",
+        color: '#3781c2',
       },
     ]
   };
